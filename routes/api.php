f12c9e040a2cef527002320e959feba182811914
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-29 15:01:48 +0200

    creating routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('signatures', 'Api\SignatureController')->only(['index', 'show', 'store']);
+Route::put('signatures/{signature}/report', 'Api\ReportSignature@update');
