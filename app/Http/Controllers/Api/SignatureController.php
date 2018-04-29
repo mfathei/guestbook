092f8f3a14511161eecaeb91130b1b2bcb41e273
Mohamed Mahdy <oracle.dev10g@gmail.com>
2018-04-29 15:19:41 +0200

    create signatures controller

---
+++ app/Http/Controllers/Api/SignatureController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Signature;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\SignatureResource;
+
+class SignatureController extends Controller
+{
+    /**
+	* Return a paginated list of signatures
+	*
+	* @return SignatureResource
+    */
+    public index(){
+    	$signatures = App::Signature::latest()->ignoreFlagged()->paginate(20);
+    	return SignatureResource::collection($signatures);
+    }
+
+    /**
+	* Fetch and return the signature
+	*
+	* @param  Signature $signature
+	* @return SignatureResource
+    */
+    public function show(Signature $signature){
+    	return new SignatureResource($signature);
+    }
+
+    /**
+    * Create and save a new signature to the database
+    *
+    * @param Request $request
+    * @return SignatureResource
+    */
+    public function store(Request $request){
+    	$signature = $this->validate([
+    		'name' => 'required|min:3|max:50',
+    		'emai' => 'required|email',
+    		'body' => 'required|min:3'
+    	]);
+
+    	$signature = Signature::create($signature);
+
+    	return new SignatureResource($signature);
+    }
+
+}
--- app/Signature.php
+++ app/Signature.php
@@ -8,4 +8,14 @@ class Signature extends Model
 {
     //
     protected $fillable = ['name', 'email', 'body', 'flagged_at'];
+
+    /**
+    * Ignore flagged signatures
+    *
+    * @param $query
+    * @return mixed
+    */
+    public function scopeIgnoreFlagged($query){
+    	return $query->where('flagged_at', null);
+    }
 }
